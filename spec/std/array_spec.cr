ce4913305f80f54921e1e68c2394546bc786edba
rogerdpack <rogerpack2005@gmail.com>
2020-04-04 23:24:54 -0600

    minor

--- spec/std/array_spec.cr
+++ spec/std/array_spec.cr
@@ -1292,7 +1292,7 @@ describe "Array" do
 
     it "sorts objects with block unstably optionally" do
       a = (1..17).to_a.map { Spaceship.new(0.0) }
-      a.sort(stable: false) { 0 }.should_not eq(a) # implementation detail but sorted out of order
+      a.sort(stable: false) { 0 }.should_not eq(a) # implementation detail but sorts out of order
     end
   end
 
@@ -1415,7 +1415,7 @@ describe "Array" do
     it "sort! objects with block unstably optionally" do
       a = (1..17).to_a.map { Spaceship.new(0.0) }
       original = a.dup
-      a.sort!(stable: false) { 0 }.should_not eq(original) # implementation detail but sorted out of order
+      a.sort!(stable: false) { 0 }.should_not eq(original) # implementation detail but sorts out of order
     end
   end
 
@@ -1461,7 +1461,7 @@ describe "Array" do
 
     it "sorts by objects with block unstably optionally" do
       a = (1..17).to_a.map { Spaceship.new(0.0) }
-      a.sort_by(stable: false) { 0 }.should_not eq(a) # implementation detail but sorted out of order
+      a.sort_by(stable: false) { 0 }.should_not eq(a) # implementation detail but sorts out of order
     end
 
     it "sorts by objects with block returning object stably by default" do
@@ -1520,7 +1520,7 @@ describe "Array" do
     it "sorts by! objects with block unstably optionally" do
       a = (1..17).to_a.map { Spaceship.new(0.0) }
       original = a.dup
-      a.sort_by!(stable: false) { 0 }.should_not eq(original) # implementation detail but sorted out of order
+      a.sort_by!(stable: false) { 0 }.should_not eq(original) # implementation detail but sorts out of order
     end
 
     it "sorts by objects with block returning object stably by default" do
